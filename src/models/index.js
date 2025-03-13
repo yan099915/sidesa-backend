f6906f17cbe065d888d9f12f72cbe68eb9713ee6
Yan Budiman <yan.septyadi@gmail.com>
2025-03-13 08:57:34 +0700

    debugging

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
+const logger = require("../common/logger.js");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require("../database/dbConfig.js")[env];
@@ -36,8 +37,12 @@ sequelize
   .authenticate()
   .then((res) => {
     console.log("CONNECTION_SUCCESS");
+    logger.info("CONNECTION_SUCCESS");
   })
-  .catch((err) => console.log("FAILED_TO_CONNECT ", err));
+  .catch((err) => {
+    logger.error("FAILED_TO_CONNECT ", err);
+    console.log("FAILED_TO_CONNECT ", err);
+  });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
